42cb118da417010ddedd882af41c14db47d4e63e
Luan Farias <luan.farias_bvs@outlook.com>
2021-06-11 01:24:57 -0300

    Maden the profile page

---
+++ amigos.php
@@ -0,0 +1,95 @@
+<?php
+
+require_once './config.php';
+require_once './models/Auth.php';
+require_once './dao/PostDaoPostgres.php';
+
+$auth = new Auth($pdo, $base);
+$userInfo = $auth->checkToken();
+$activeMenu = 'friends';
+
+$id = filter_input(INPUT_GET, 'id');
+if (!$id || empty($id))
+{
+    $id = $userInfo->getId();
+}
+
+$postDao = new PostDaoPostgres($pdo);
+$userDao = new UserDaoPostgres($pdo);
+
+$user = $userDao->findById($id, true);
+
+if (!$user)
+{
+    header('Location: ' . $base);
+    exit;
+}
+
+if ($user->getId() !== $userInfo->getId()) {
+    $activeMenu = '';
+}
+
+require './partials/header.php';
+require './partials/aside.php';
+
+?>
+<section class="feed">
+    <?php
+    require './partials/profile-header.php';
+    ?>
+
+    <div class="row">
+        <div class="column">
+            <div class="box">
+                <div class="box-body">
+                    <div class="tabs">
+                        <div class="tab-item" data-for="followers">
+                            Seguidores
+                        </div>
+                        <div class="tab-item active" data-for="following">
+                            Seguindo
+                        </div>
+                    </div>
+                    <div class="tab-content">
+                        <div class="tab-body" data-item="followers">
+                            <div class="full-friend-list">
+                                <?php foreach($user->getFollowers() as $follower): ?>
+                                <div class="friend-icon">
+                                    <a href="<?= $base; ?>/perfil.php?id=<?= $follower->getId(); ?>">
+                                        <div class="friend-icon-avatar">
+                                            <img src="<?= $base; ?>/media/avatars/<?= $follower->getAvatar(); ?>" />
+                                        </div>
+                                        <div class="friend-icon-name">
+                                            <?= $follower->getName(); ?>
+                                        </div>
+                                    </a>
+                                </div>
+                                <?php endforeach; ?>
+                            </div>
+                        </div>
+
+                        <div class="tab-body" data-item="following">
+                            <div class="full-friend-list">
+                                <?php foreach($user->getFollowing() as $following): ?>
+                                <div class="friend-icon">
+                                    <a href="<?= $base; ?>/perfil.php?id=<?= $following->getId(); ?>">
+                                        <div class="friend-icon-avatar">
+                                            <img src="<?= $base; ?>/media/avatars/<?= $following->getAvatar(); ?>" />
+                                        </div>
+                                        <div class="friend-icon-name">
+                                            <?= $following->getName(); ?>
+                                        </div>
+                                    </a>
+                                </div>
+                                <?php endforeach; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+require './partials/footer.php';
+?>
--- perfil.php
+++ perfil.php
@@ -36,59 +36,9 @@ require './partials/aside.php';
 
 ?>
 <section class="feed">
-    <div class="row">
-        <div class="box flex-1 border-top-flat">
-            <div class="box-body">
-                <div
-                    class="profile-cover"
-                    style="
-                        background-image: url('<?= $base; ?>/media/covers/<?= $user->getCover(); ?>');
-                    "
-                ></div>
-                <div class="profile-info m-20 row">
-                    <div class="profile-info-avatar">
-                        <img src="<?= $base; ?>/media/avatars/<?= $user->getAvatar(); ?>" />
-                    </div>
-                    <div class="profile-info-name">
-                        <div class="profile-info-name-text">
-                            <?= $user->getName(); ?>
-                        </div>
-                        <?php if(!empty($user->getCity())): ?>
-                        <div class="profile-info-location">
-                            <?= $user->getCity(); ?>
-                        </div>
-                        <?php endif; ?>
-                    </div>
-                    <div class="profile-info-data row">
-                        <div class="profile-info-item m-width-20">
-                            <div class="profile-info-item-n">
-                                <?= count($user->getFollowers()); ?>
-                            </div>
-                            <div class="profile-info-item-s">
-                                Seguidores
-                            </div>
-                        </div>
-                        <div class="profile-info-item m-width-20">
-                            <div class="profile-info-item-n">
-                                <?= count($user->getFollowing()); ?>
-                            </div>
-                            <div class="profile-info-item-s">
-                                Seguindo
-                            </div>
-                        </div>
-                        <div class="profile-info-item m-width-20">
-                            <div class="profile-info-item-n">
-                                <?= count($user->getPhotos()); ?>
-                            </div>
-                            <div class="profile-info-item-s">
-                                Fotos
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php
+    require './partials/profile-header.php';
+    ?>
 
     <div class="row">
         <div class="column side pr-5">
